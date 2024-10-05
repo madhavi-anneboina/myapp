913dc370c5519d200dfc13a1477329befe1565cc
Madhavi Anneboina <madhavi.anneboina@gmail.com>
2024-10-04 21:40:42 -0400

    added template literal

--- src/components/StyleSheet.js
+++ src/components/StyleSheet.js
@@ -5,7 +5,7 @@ import "./myStyles.css";
 const StyleSheet = ({primary}) => {
     let className = primary ? "primary" : ""
   return (
-    <div className={className}>
+    <div className = {`${className} font-xl`}>
        <h1>StyleSheet</h1>
         </div>
   )
