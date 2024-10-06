ecba5312dfa8b6c738eb438aed433431b6b3a708
Madhavi Anneboina <madhavi.anneboina@gmail.com>
2024-10-05 21:17:44 -0400

    aded changeHandler

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
  class Form extends Component {
+    constructor(){
+     super()
+     this.state={
+        username:''
+     }
+    }
+  chageHandler = (event) =>{
+    this.setState({username :event.target.value})
+  }
   render() {
     return (
       <div>
          <lable>UserName</lable>
-         <input type="text" />
+         <input type="text" value={this.state.username} onChange={this.chageHandler}/>
       </div>
     )
   }
