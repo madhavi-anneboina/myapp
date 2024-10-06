859fe7bc88b1038e1ea63d924e6605727adab28b
Madhavi Anneboina <madhavi.anneboina@gmail.com>
2024-10-05 21:32:10 -0400

    added form ele

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,49 @@ import React, { Component } from 'react'
     constructor(){
      super()
      this.state={
-        username:''
+        username:'',
+        comments:'',
+        topic:'react'
      }
     }
   chageHandler = (event) =>{
-    this.setState({username :event.target.value})
+    this.setState({
+        username :event.target.value,
+       
+    })
+  }
+  handleCommentChange = (event) =>{
+    this.setState({
+        comments :event.target.value,
+       
+    })
+  }
+  chageSelector = (event) =>{
+    this.setState({
+        topic:this.state.value
+    })
   }
   render() {
     return (
       <div>
+        <div>
          <lable>UserName</lable>
          <input type="text" value={this.state.username} onChange={this.chageHandler}/>
+         </div>
+         <div>
+            <lable>Comments</lable>
+            <textarea value={this.state.comments} onChange={this.handleCommentChange}> </textarea>
+         </div>
+         <div>
+            <label>Topic</label>
+            <select value={this.state.topic} onChange = {this.chageSelector}>
+                <option value='react'>React</option>
+                <option value='angular'>Angular</option>
+                <option value='vue'>Vue</option>
+            </select>
+         </div>
       </div>
+
     )
   }
 }
