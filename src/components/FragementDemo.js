1b74d4ddfe17c4a5c5bc5d8deba26c606e56d071
Madhavi Anneboina <madhavi.anneboina@gmail.com>
2024-10-06 21:02:39 -0400

    added react.fragement

---
+++ src/components/FragementDemo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FragementDemo = () => {
+  return (
+    <React.Fragment>
+        <h1> FragementDemo</h1>
+        <p>welcome to fragement demo component</p>
+    </React.Fragment>
+  )
+}
+
+export default FragementDemo
---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Colums from './Colums';
+
+const Table = () => {
+  return (
+    <table>
+        <tbody>
+            <tr>
+                <Colums/>
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+export default Table;
